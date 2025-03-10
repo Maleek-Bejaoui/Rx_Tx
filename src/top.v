1bbea0d3d9407c601aab5a4116b9e2dadfca75b6
Maleek <101218361+Maleek-Bejaoui@users.noreply.github.com>
2025-03-10 16:04:37 +0100

    Update top.v

--- src/top.v
+++ src/top.v
@@ -24,13 +24,23 @@ module tt_um_top (
   // List all unused inputs to prevent warnings
 //  wire _unused = &{ena, clk, rst_n, 1'b0};
 //  wire _unused = &{ena, 1'b0};
+// Déclaration des signaux intermédiaires
+ 
+    wire data_valid;
 
+  // Affectation correcte de uio_out et uio_oe
+  assign uio_out[0] = data_valid;  // On met le signal valid sur uio_out[0]
+  assign uio_oe[0]  = 1'b1;        // On active la sortie uio_out[0] en sortie
+  assign uio_out[7:1] = 7'b0;      // Les autres sorties restent à 0
+  assign uio_oe[7:1]  = 7'b0;      // Les autres broches restent en entrée
+
+  // Instanciation de topVHDL
   topVHDL m_topVHDL (
-    .clk  (clk),    // Clock input
-      .ena_in  (ena), // Reset input
-      .data_in (ui_in) , // 8-bit counter input
-      .data_out (uo_out),
-      .data_valid (uio_out)
+    .clk       (clk),     // Clock input
+    .ena_in    (ui_in[0]), // Activation (au lieu de ena qui est toujours 1)
+    .data_in   (ui_in),   // 8-bit Data input
+    .data_out  (uo_out),  // 8-bit Data output
+    .data_valid(data_valid) // Data valid signal
   );
 
 endmodule
